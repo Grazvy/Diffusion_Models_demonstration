c71e394a3f852743241c1203e697d8485f4bc729
Grazv <grazvydas.kuras@gmail.com>
2024-06-28 20:45:21 +0200

    utility methods for uNetManager

--- utils2.py
+++ utils2.py
@@ -1,3 +1,4 @@
+import json
 import matplotlib.pyplot as plt
 from dataLoader import get_dataloader, inverse_transform
 from utils1 import make_grid
@@ -15,3 +16,32 @@ def visualize_data(dataset_name="MNIST", amount=72):
         plt.imshow(grid_img.permute(1, 2, 0))
         plt.axis("off")
         break
+
+
+def read_config(file_path):
+    with open(file_path + '.json', 'r') as f:
+        return json.load(f)
+
+
+def write_config(file_path, config):
+    with open(file_path + '.json', 'w') as f:
+        json.dump(config, f, indent=4)
+
+
+def yes_no_prompt(message):
+    while True:
+        response = input(message + " (y/n): ").strip().lower()
+        if response == 'y' or response == 'n':
+            return response
+        else:
+            print("Please enter 'y' for yes or 'n' for no.")
+
+
+def add_tar_suffix(checkpoint_name):
+    if checkpoint_name is None:
+        return None
+
+    if checkpoint_name.endswith(".tar"):
+        return checkpoint_name
+
+    return checkpoint_name + ".tar"
